1e58ca5baea7b5a29305c1b4d080c20848ea5f28
Camieux <tarsisiusadhi@gmail.com>
2022-07-06 22:16:09 +0700

    setup admin-bra

--- bra_crud/config/database.php
+++ bra_crud/config/database.php
@@ -1,3 +1,3 @@
 <?php
 
-$db = mysqli_connect('localhost', 'camieux', 'viper666', 'bumira');
+$db = mysqli_connect('localhost', 'root', '', 'bumira');
--- bumira.sql
+++ bumira.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.1.1deb5ubuntu1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Waktu pembuatan: 06 Jul 2022 pada 06.56
--- Versi server: 10.4.8-MariaDB
--- Versi PHP: 7.1.33
+-- Host: localhost:3306
+-- Generation Time: Jul 06, 2022 at 10:14 PM
+-- Server version: 8.0.28-0ubuntu4
+-- PHP Version: 8.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,88 +24,71 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `akun_admin`
+-- Table structure for table `akun_admin`
 --
 
 CREATE TABLE `akun_admin` (
-  `userID` int(11) NOT NULL,
-  `namaLengkap` varchar(100) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `userID` int NOT NULL,
+  `namaLengkap` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `role` varchar(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data untuk tabel `akun_admin`
+-- Dumping data for table `akun_admin`
 --
 
 INSERT INTO `akun_admin` (`userID`, `namaLengkap`, `username`, `password`, `role`) VALUES
-(1, 'admin', 'admin', 'admin', '1'),
-(2, 'rizky', 'rizky', 'rizky', '2'),
-(5, 'test123', 'test123', '$2y$10$A4RID/NRe/6UcUS/vGgpMe0l6VA8DszuwY.4Cprumqp', '1'),
-(6, 'rizky123', 'rizky123', '$2y$10$alNmClkKJrk.8PJZcBIOne7fug3objXXfAdzPxZYz5r', '2'),
-(7, '123456', '123456', '$2y$10$SNwsYszDsyl6Q5mpdPNC4OOrxKLqs4HQqCiYvIMI46U', '1');
+(1, 'admin', 'admin', 'password', '1');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `produk`
+-- Table structure for table `produk`
 --
 
 CREATE TABLE `produk` (
-  `id_produk` int(11) NOT NULL,
+  `id_produk` int NOT NULL,
   `nama_produk` varchar(100) NOT NULL,
   `tipe` varchar(100) NOT NULL,
   `lokasi` varchar(100) NOT NULL,
   `gambar` blob NOT NULL,
   `deskripsi` varchar(255) NOT NULL,
-  `tanggal` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `produk`
---
-
-INSERT INTO `produk` (`id_produk`, `nama_produk`, `tipe`, `lokasi`, `gambar`, `deskripsi`, `tanggal`) VALUES
-(1, 'RUMAH KU LAKU', 'LUXURY', 'PLUTO', '', 'test123', '2022-07-02 15:33:22'),
-(2, 'RUMAH MU', 'HOT', 'uranus', 0x436170747572652e4a5047, 'wow mahal banget', '2022-07-02 15:19:49'),
-(4, 'adsfawe', 'sadfeaw', 'sadfawe', 0x436170747572652e4a5047, 'sadfawe', '2022-07-02 15:19:49'),
-(5, 'aaaaaa', 'sadfeaw', 'sadfawe', '', '', '2022-07-02 15:19:49'),
-(6, 'aaaaaa', 'sadfeaw', 'sadfawe', '', 'aaaa', '2022-07-02 15:19:49'),
-(7, 'aaaaaa', 'sadfeaw', 'sadfawe', '', 'asdwasd', '2022-07-02 15:19:49'),
-(8, '12345', 'sadfeaw', 'sadfawe', '', 'asdasd', '2022-07-02 18:13:02');
+  `tanggal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `akun_admin`
+-- Indexes for table `akun_admin`
 --
 ALTER TABLE `akun_admin`
   ADD PRIMARY KEY (`userID`);
 
 --
--- Indeks untuk tabel `produk`
+-- Indexes for table `produk`
 --
 ALTER TABLE `produk`
   ADD PRIMARY KEY (`id_produk`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `akun_admin`
+-- AUTO_INCREMENT for table `akun_admin`
 --
 ALTER TABLE `akun_admin`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `userID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT untuk tabel `produk`
+-- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_produk` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
